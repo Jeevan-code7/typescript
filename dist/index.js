d839a11b6987c0d9479b7989a192304c9cafecaa
jeevan <jk29102023@gmail.com>
2026-06-01 16:45:48 +0530

    tytpescript set up done

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Levi is here";
+function greet(user) {
+    return `Hello ${user}`;
+}
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const name: string = "Levi is here lets go with typescript";
+
+function greet(user: string): string {
+  return `Hello ${user}`;
+}
+
+console.log(greet(name));
